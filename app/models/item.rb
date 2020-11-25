8aa39eaafcfab9e04fac3d392e8a5cc1b4f8a971
kiki0718 <71147023+kiki0718@users.noreply.github.com>
2020-11-25 19:11:59 +0900

    validationの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   
-  validates :name, :price, :category_id, :status_id, :delivery_fee,
-            :prefecture_id, :ship_day_id, :description,
+  validates :name, :category_id, :status_id, :delivery_fee,
+            :prefecture_id, :ship_day_id, :description, :image, presence: true
+  validates :price format: { with: /\A[-]?[0-9]+(\.[0-9]+)?\z/ }, inclusion: {in: 300..9999999}, presence: true
 end
