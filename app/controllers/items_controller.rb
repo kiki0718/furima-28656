35d84fe36e648f577e9657af6e57493d664f6e0e
kiki0718 <71147023+kiki0718@users.noreply.github.com>
2020-12-13 20:29:26 +0900

    出品者のログインの確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :login_check, only: [:destroy]
 
   def new
     @item = Item.new
@@ -31,5 +32,11 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :price, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :ship_day_id, :description, :image).merge(user_id: current_user.id)
+  end
+
+  def login_check
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
