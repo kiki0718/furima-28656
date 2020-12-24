41682114d0a9593a353682da449a1ba436fd8d02
kiki0718 <71147023+kiki0718@users.noreply.github.com>
2020-12-25 05:07:15 +0900

    テーブルへの保存処理

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -9,5 +9,11 @@ class Purchase
     validates :phone_number, format: {with: /\A\d{11}\z/}
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-  
+
+  def save
+    user = User.create
+    item = Item.create
+    Purchase_Records.create(user: user.id, item: item.id)
+    Address.create(purchse_record_id: purchse_record.id, postal_code: postal_code, prefecture_id: prefecture_id, municipatities: municipatities, buildeing_name: building_name, house_number: house_number, phone_number: phone_number)
+  end
 end
