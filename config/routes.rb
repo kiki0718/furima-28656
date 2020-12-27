131f35fcd1e1171bf646279578bf91da64c38e1d
kiki0718 <71147023+kiki0718@users.noreply.github.com>
2020-12-27 14:53:38 +0900

    orderコントローラーのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
-  resources :items, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :items, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :order, only: [:index, :create]
+  end
 end
